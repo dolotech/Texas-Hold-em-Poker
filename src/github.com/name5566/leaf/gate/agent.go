e49c7a3924acb2484076dd24399b5412e04e181c
michael <dolotech@163.com>
2018-04-12 00:51:06 +0800

    add leaf

---
+++ src/github.com/name5566/leaf/gate/agent.go
@@ -0,0 +1,15 @@
+package gate
+
+import (
+	"net"
+)
+
+type Agent interface {
+	WriteMsg(msg interface{})
+	LocalAddr() net.Addr
+	RemoteAddr() net.Addr
+	Close()
+	Destroy()
+	UserData() interface{}
+	SetUserData(data interface{})
+}
